f00928bfde290d2c806076a6f98213d908d90255
mdimran1409036 <mdimran1409036@gmail.com>
2023-04-01 03:58:10 +0600

    authentication added

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ import cookieParser from "cookie-parser";
 dotenv.config();
 import { Message } from "./model/messageModel.js";
 import { User } from "./model/userModel.js";
-
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 const port = process.env.PORT || 3000;
 
 mongoose
@@ -25,32 +26,70 @@ app.use(express.static(path.join(path.resolve(), "public")));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
-const isAuthenticated = (req, res, next) => {
+const isAuthenticated = async (req, res, next) => {
     const { token } = req.cookies;
     if (token) {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const user = await User.findById({ _id: decoded._id });
+        req.user = user;
         next();
     } else {
-        res.render("login");
+        res.redirect("/login");
     }
 };
 // routes
 
-app.get("/login", (req, res) => {
-    res.render("login");
+app.get("/register", (req, res) => {
+    res.render("register");
 });
-
-app.post("/login", async (req, res) => {
-    const { email, password } = req.body;
+app.post("/register", async (req, res) => {
+    const { name, email, password } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
     try {
-        const user = await User.create({ email, password });
-        res.cookie("token", user._id, {
+        let user = await User.findOne({ email });
+        if (user) {
+            return res.redirect("/login");
+        }
+        user = await User.create({ name, email, password: hashedPassword });
+        const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET);
+
+        res.cookie("token", token, {
             httpOnly: true,
             expires: new Date(Date.now() + 60000),
         });
         res.redirect("/");
     } catch (error) {
-        console.log(error)
-        res.json({message: error._message})
+        console.log(error);
+        res.json({ error });
+    }
+});
+app.get("/login", (req, res) => {
+    res.render("login");
+});
+app.post("/login", async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.redirect("/register");
+        }
+        const isPasswordMatched = await bcrypt.compare(password, user.password);
+        if (!isPasswordMatched)
+            return res.render("login", {
+                email,
+                message: "Incorrect password",
+            });
+
+        const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET);
+
+        res.cookie("token", token, {
+            httpOnly: true,
+            expires: new Date(Date.now() + 60000),
+        });
+        res.redirect("/");
+    } catch (error) {
+        console.log(error);
+        res.json({ message: error._message });
     }
 });
 
@@ -62,7 +101,7 @@ app.post("/logout", (req, res) => {
 });
 
 app.get("/", isAuthenticated, (req, res) => {
-    res.render("logout");
+    res.render("logout", { user: req.user });
 });
 
 app.get("/users", (req, res) => {
